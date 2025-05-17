58018299fc08eaf79d96df2247ae7d3189b08ace
Sachin-05 <51017352+Sachin-05@users.noreply.github.com>
2025-05-17 15:12:35 +0530

    Create sortcolors.cpp

---
+++ sortcolors.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       // sort(nums.begin(), nums.end());
+
+       // now without sorting
+       int zerocnt = 0;
+       int onecnt = 0;
+       int twocnt = 0;
+
+       for(int i=0; i<nums.size(); ++i){
+        if(nums[i]==0){
+            zerocnt++;
+        }
+
+        if(nums[i]==1){
+            onecnt++;
+        }
+
+        if(nums[i]==2){
+            twocnt++;
+        }
+       }
+
+       for(int i=0; i<zerocnt; ++i){
+            nums[i] = 0;
+       }
+
+       for(int i=zerocnt; i<zerocnt+onecnt; ++i){
+            nums[i] = 1;
+       }
+
+       for(int i=zerocnt+onecnt; i<twocnt+onecnt+zerocnt; ++i){
+            nums[i] = 2;
+       }
+       
+    }
+};
