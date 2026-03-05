c3b5e28b35d00b6f28eef8e8bdec8caf3ceb3529
Sachin-05 <51017352+Sachin-05@users.noreply.github.com>
2026-03-06 00:56:43 +0530

    Create minimumchangestomakestringalternate.cpp

---
+++ minimumchangestomakestringalternate.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(string s) {
+            int n = s.length();
+    int startWith0 = 0;
+    int startWith1 = 0;
+
+    for(int i = 0; i < n; i++) {
+        char expected0 = (i % 2 == 0) ? '0' : '1';
+        char expected1 = (i % 2 == 0) ? '1' : '0';
+
+        if(s[i] != expected0)
+            startWith0++;
+
+        if(s[i] != expected1)
+            startWith1++;
+    }
+
+    return min(startWith0, startWith1);
+    }
+};
